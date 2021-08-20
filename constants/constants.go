726e888a2e99501173c271cc0a30a4eb1ed964ff
Akshat <akshatmittal93@gmail.com>
2021-08-20 21:31:50 +0530

    Added timeout for torrent files

--- constants/constants.go
+++ constants/constants.go
@@ -15,7 +15,7 @@ const (
 
 // messages
 const (
-	WELCOME_MSG          = "Welcome to TorrentGenie \xF0\x9F\x98\x81	 - Get torrent magnet links \n\n Search torrents by typing a name"
+	WELCOME_MSG          = "Welcome to TorrentGenie \xF0\x9F\x98\x81	 - Get torrent files | magnet links \n\n Start by typing a name"
 	INVALID_REPLY        = "Invalid reply, please try again"
 	SOMETHING_WENT_WRONG = "Something went wrong, please try again"
 	NO_RESULTS           = "No torrents found"
